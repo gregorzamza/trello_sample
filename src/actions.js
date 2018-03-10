6e06d09a1fafac62c1cdbb446672695d5619ec6a
grgrzamza <imaginenozamza@gmail.com>
2018-03-10 16:53:41 +0300

    Actions for boards

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,8 @@ export const ADD_TODOLIST = 'ADD_TODOLIST'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 export const REMOVE_TODO = 'REMOVE_TODO'
 export const REMOVE_TODOLIST = 'REMOVE_TODOLIST'
+export const ADD_TODOBOARD = 'ADD_TODOBOARD'
+export const REMOVE_TODOBOARD = 'REMOVE_TODOBOARD'
 
 export function addTodo(text){
   return {type: ADD_TODO, payload: {text}, error: false};
@@ -12,14 +14,22 @@ export function addTodoList(text){
   return {type: ADD_TODOLIST, payload: {text}, error: false};
 }
 
-export function toggleTodo(index){
-  return {type: TOGGLE_TODO, payload: {index}, error: false};
+export function toggleTodo(id){
+  return {type: TOGGLE_TODO, payload: {id}, error: false};
 }
 
-export function removeTodo(index){
-  return {type: REMOVE_TODO, payload: {index}, error: false};
+export function removeTodo(id){
+  return {type: REMOVE_TODO, payload: {id}, error: false};
 }
 
-export function removeTodoList(index){
-  return {type: REMOVE_TODOLIST, payload: {index}, error: false}
+export function removeTodoList(id){
+  return {type: REMOVE_TODOLIST, payload: {id}, error: false}
+}
+
+export function addTodoBoard(text){
+  return {type: ADD_TODOBOARD, payload: {text}, error: false};
+}
+
+export function removeTodoBoard(id){
+  return {type: REMOVE_TODOBOARD, payload: {id}, error: false}
 }
